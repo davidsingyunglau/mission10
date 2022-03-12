f222407d8017c9c5b261beeaa31ecb31cd6f4b83
davidschool <davidsingyunglau@gmail.com>
2022-03-11 18:51:56 -0700

    commit 1

--- WaterProject/Models/EFBookstoreRepository.cs
+++ WaterProject/Models/EFBookstoreRepository.cs
@@ -7,11 +7,29 @@ namespace WaterProject.Models
     {
         private BookstoreContext context { get; set; }
 
-        public EFBookstoreRepository (BookstoreContext temp)
+        public EFBookstoreRepository(BookstoreContext temp)
         {
             context = temp;
         }
 
         public IQueryable<Books> Books => context.Books;
+
+        public void SaveBook(Books b)
+        {
+            context.SaveChanges();
+        }
+
+        public void CreateBook(Books b)
+        {
+            context.Add(b);
+            context.SaveChanges();
+        }
+
+        public void DeleteBook(Books b)
+        {
+            context.Remove(b);
+            context.SaveChanges();
+        }
     }
+
 }
